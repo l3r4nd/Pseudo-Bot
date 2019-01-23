e2aac6ea5cb83cf7a0a630399fb894e246c93dd1
Prasad Joshi <32058627+l3r4nd@users.noreply.github.com>
2019-01-23 21:53:25 +0530

    Update run_game.bat

--- run_game.bat
+++ run_game.bat
@@ -1,1 +1,1 @@
-halite.exe --replay-directory replays/ -vvv --width 64 --height 64 --seed 1547587835 "python PseudoBotV7.py" "python PseudoBotV6.py"
+halite.exe --replay-directory replays/ -vvv --width 64 --height 64 --seed 1547587835 "python PseudoBot.py" "python PseudoBot.py"
